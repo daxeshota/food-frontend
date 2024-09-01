2077a78d605b4d4d387373f85de9fd40e4048dbd
daxeshota <daxeshota@gmail.com>
2024-09-01 17:47:44 +0530

    assets and navbar added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourote food here</h2>
+            <p>Simple paragraph</p>
+            <button>view menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
